818ee88783a5869f4adffa09a769489a1b70352f
Erick Estrada <erickestradapj@gmail.com>
2021-12-02 01:01:28 -0500

    deploy on heroku

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const { dbConnection } = require('./database/config');
 require('dotenv').config();
 
@@ -19,6 +20,11 @@ app.use(express.json());
 
 // Routes
 app.use('/api/auth', require('./routes/auth'));
+
+// Other routes
+app.get('*', (req, res) => {
+   res.sendFile(path.resolve(__dirname, 'public/index.html'));
+});
 
 app.listen(process.env.PORT, () => {
    console.log(`App listening at http://localhost:${process.env.PORT}`);
